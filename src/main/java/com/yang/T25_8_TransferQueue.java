6fbb333f4b2a998231086c4ed45a2551ee9451af
yangxiangyang <1161108412@qq.com>
2019-08-07 10:01:34 +0800

    延时队列

--- src/main/java/com/yang/T25_8_TransferQueue.java
+++ src/main/java/com/yang/T25_8_TransferQueue.java
@@ -4,7 +4,11 @@ package com.yang;
 import java.util.concurrent.LinkedTransferQueue;
 
 public class T25_8_TransferQueue {
-
+	//JDK1.7才添加的阻塞队列，基于链表实现的FIFO无界阻塞队列
+//		是ConcurrentLinkedQueue（循环CAS+volatile 实现的wait-free并发算法）、
+//		SynchronousQueue（公平模式下转交元素）、
+//		LinkedBlockingQueue（阻塞Queue的基本方法）的超集。
+//		而且LinkedTransferQueue更好用，因为它不仅仅综合了这几个类的功能，同时也提供了更高效的实现
 	public static void main(String[] args) throws InterruptedException {
 		LinkedTransferQueue<String> strings = new LinkedTransferQueue<String>();
 		
@@ -17,8 +21,9 @@ public class T25_8_TransferQueue {
 		}).start();*/
 		
 		//strings.transfer("aaa");
-		
-		strings.put("aaa");
+		// 1-消费者先启动，生产者启动，先去找有没有消费者，有则消费，无则添加到队列
+		// 2-生产者先启动，没有消费者则阻塞
+		strings.put("aaa");//使用put add offer均不会阻塞
 		
 		new Thread(()->{
 			try {
